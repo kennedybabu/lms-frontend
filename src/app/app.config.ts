fc28f0e845e7d3cd5af5f3f17b140e2a28a460a0
kennedybabu <kennedynjogu8@gmail.com>
2026-01-16 04:03:27 +0300

    ft: add tailwind, primeng and basic folder structure

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,7 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { providePrimeNG } from 'primeng/config';
+import Lara from '@primeuix/themes/lara'
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
@@ -8,6 +10,14 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes), provideClientHydration(withEventReplay()),
+    providePrimeNG({
+      theme: {
+        preset: Lara,
+        options: {
+            darkModeSelector: false || 'none'
+        }
+      }
+    })
   ]
 };
